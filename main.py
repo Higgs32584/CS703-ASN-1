a5492c689dcd9c5bcef557f3f6a5cbbb9a9e69c3
Michael Higgins <55243596+Higgs32584@users.noreply.github.com>
2021-10-22 10:08:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from matplotlib import pyplot as plt
+
+
 def blank_check(y):
     if y == "\n" or y == " ":
         return True
     return False
+
+
 def readtolist(filepath):
     f = open(filepath, "r")
     strbuild = ""
@@ -11,35 +15,37 @@ def readtolist(filepath):
         for y in x:
             if blank_check(y):
                 data.append(int(strbuild))
-                strbuild=""
+                strbuild = ""
             elif blank_check(y):
                 data.append(int(strbuild))
-                strbuild=""
+                strbuild = ""
             else:
-                strbuild = strbuild+y
+                strbuild = strbuild + y
     if strbuild != "":
         data.append(int(strbuild))
-    
+
     print(data)
-    
+
     return data
 
-data = readtolist("/data/CS703/A1/test.txt")
+ydata = readtolist("C:/Users/mjhig/PycharmProjects/pythonProject1/ages.txt")
+xdata = readtolist("C:/Users/mjhig/PycharmProjects/pythonProject1/years.txt")
 
-ydata=readtolist("/data/CS703/A1/ages.txt")
-xdata=readtolist("/data/CS703/A1/years.txt")
-
-plt.plot(xdata,ydata,color='blue',marker='o',linestyle='dotted')
+plt.plot(xdata, ydata, color='blue', marker='o', linestyle='dotted')
 plt.title("Ages Over Time")
 plt.xlabel("Years")
 plt.ylabel("Ages")
 
-xdata_fil=[]
-ydata_fil=[]
-c=[10,20,30,40,50]
+
+xdata_fil = []
+ydata_fil = []
+c = [10, 20, 30, 40, 50]
 for x in range(len(xdata)):
-    if(xdata[x] < 2021 and xdata[x] > 2000):
+    if (xdata[x] < 2021 and xdata[x] > 2000):
         xdata_fil.append(xdata[x])
         ydata_fil.append(ydata[x])
 
-plt.scatter(xdata_fil, ydata_fil,marker='o',linestyle='solid')
+plt.scatter(xdata_fil, ydata_fil, marker='o', linestyle='solid')
+
+plt.show()
+
